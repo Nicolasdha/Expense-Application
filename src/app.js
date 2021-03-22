3394b5fb9f6afef0e689ad5741c401159527257c
Nicolasdha <sankp001@gmail.com>
2021-03-22 12:05:17 -0600

    Cleaned code

--- src/app.js
+++ src/app.js
@@ -19,8 +19,6 @@ const store = configureStore();
 
 const unsubscribe = store.subscribe(() =>{ 
     const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    // console.log(visibleExpenses) 
 })
 
 
